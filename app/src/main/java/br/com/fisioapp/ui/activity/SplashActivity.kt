d45a936492f8f96a6c44b04fef27de6e0f111cf8
jvictororiz <joao.victor.roriz2016@gmail.com>
2019-12-17 00:42:10 -0300

    finalizando login

--- app/src/main/java/br/com/fisioapp/ui/activity/SplashActivity.kt
+++ app/src/main/java/br/com/fisioapp/ui/activity/SplashActivity.kt
@@ -1,24 +1,25 @@
 package br.com.fisioapp.ui.activity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import br.com.fisioapp.R
 import br.com.fisioapp.ui.base.BaseActivity
-import br.com.fisioapp.viewModel.LoginViewModel
 import br.com.fisioapp.viewModel.SplashViewModel
 
 class SplashActivity : BaseActivity() {
     private val viewModel: SplashViewModel by lazy { ViewModelProvider(this).get(SplashViewModel::class.java) }
-
+    private val TIME_TO_END_SPLASH = 700L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        viewModel.findStatus()
         subscribe()
+        viewModel.findStatus()
+            Handler().postDelayed({
+        }, TIME_TO_END_SPLASH)
     }
 
     private fun subscribe() {
